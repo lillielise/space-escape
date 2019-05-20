b4a09a96675d0d3eb2792c241ad010a08e121d4c
NaamaBarIlan <barilan.naama@gmail.com>
2019-05-20 16:18:56 -0700

    JS outline

---
+++ js/app.js
@@ -0,0 +1,80 @@
+'use strict';
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//// DATA ////
+
+// Global Varriables //
+
+// DOM
+var gameStartButton = document.getElementById('');
+
+// DOM - Shapes
+var leftStone = document.getElementById('');
+var middleStone = document.getElementById('');
+var rightStone = document.getElementById('');
+var spaceshipStone = document.getElementById('');
+
+// Local Storage //
+
+// Connects to index.html for user input
+// Connects to scoreboard.html to assign the data  
+
+    // check if there is anything in the LS = if/else
+    // If there's data in LS
+        // *retrieve* data from LS
+        // *parse* the LS data
+        // *assign* that data to the place it will be used - scoreboard.html
+    // else - create instances to paint the page
+
+// Constructors //
+
+// Instances //
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//// FUNCTION DECLARATIONS ////
+
+// Helper Functions //
+
+// Event Handlers //
+
+// Handle 'Start Game' button click
+
+// Shape events //
+
+// Handle left stone click
+    // after user clicks on stone - open P1 HTML
+    // 
+
+// Handle middle stone click
+
+// Handle right stone click
+
+// Handle spaceship click
+    // Last game event if/else
+    // If user got the correct answers - You Win
+        // Save score to LS
+        // Assign score to scoreboard
+    // else - Game Over
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//// EXECUTABLE CODE ////
+
+// Anything that runs on page load //
+
+// Function Calls //
+
+// Event listeners //
+
+// 'Start Game' button event listener
+
+
+// Shape events: //
+
+// Click on left stone event listener
+
+// Click on middle stone event listener
+
+// Click on right stone event listener
+
+// Click on spaceship event listener
